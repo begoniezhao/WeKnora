cf1c1d597dbc6f69790f1c7b849c9ffd7629daad
wizardchen <wizardchen@tencent.com>
2026-04-22 21:24:08 +0800

    feat: Remove outdated wiki and indexing migration files
    - Deleted migration files for the wiki_pages and wiki_page_issues tables, as well as the indexing_strategy column from knowledge_bases, to clean up the schema.
    - This removal streamlines the migration process and ensures that only relevant migrations are retained in the project.

--- migrations/versioned/000036_wiki_pages.down.sql
+++
@@ -1,9 +0,0 @@
--- Migration: 000036_wiki_pages (rollback)
--- Description: Remove wiki_pages table and wiki_config column from knowledge_bases.
-DO $$ BEGIN RAISE NOTICE '[Migration 000036] Removing wiki_pages table and wiki_config column'; END $$;
-
-DROP TABLE IF EXISTS wiki_pages;
-
-ALTER TABLE knowledge_bases DROP COLUMN IF EXISTS wiki_config;
-
-DO $$ BEGIN RAISE NOTICE '[Migration 000036] wiki_pages table and wiki_config column removed successfully'; END $$;
--- migrations/versioned/000038_add_indexing_strategy.down.sql
+++
@@ -1,7 +0,0 @@
--- Migration: 000038_add_indexing_strategy (down)
--- Description: Remove indexing_strategy column from knowledge_bases.
-DO $$ BEGIN RAISE NOTICE '[Migration 000038] Dropping indexing_strategy column from knowledge_bases'; END $$;
-
-ALTER TABLE knowledge_bases DROP COLUMN IF EXISTS indexing_strategy;
-
-DO $$ BEGIN RAISE NOTICE '[Migration 000038] indexing_strategy column dropped successfully'; END $$;
